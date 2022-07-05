880af418b2165b7fe647b49156056833dc37e03a
Hua Chen <huachen724@gmail.com>
2022-07-05 04:59:37 +0000

    Implement Datastore into Project

---
+++ src/main/java/com/google/sps/servlets/CreateListServlet.java
@@ -0,0 +1,46 @@
+
+package main.java.com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+
+/* List Table
+    private final TimeStamp notifyTime;
+    private final string listName;
+    private final string username;
+*/
+
+@WebServlet("/create-list")
+public class CreateListServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      // Sanitize user input to remove HTML tags and JavaScript.
+      String list = Jsoup.clean(request.getParameter("listName"), Whitelist.none());
+      long timestamp = System.currentTimeMillis();
+      String username = Jsoup.clean(request.getParameter("username"), Whitelist.none());
+  
+      Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+      KeyFactory keyFactory = datastore.newKeyFactory().setKind("List");
+      FullEntity listEntity =
+          Entity.newBuilder(keyFactory.newKey())
+              .set("listName", list)
+              .set("notifyTime", timestamp)
+              .set("username", username)
+              .build();
+      datastore.put(listEntity);
+  
+      response.sendRedirect("/index.html");
+    }
+}
+
---
+++ src/main/java/com/google/sps/servlets/UpdateListServlet.java
@@ -0,0 +1,24 @@
+package main.java.com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/update-list")
+public class UpdateListServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      String habitName = String.parseString(request.getParameter("habitName"));
+  
+      Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+      KeyFactory keyFactory = datastore.newKeyFactory().setKind("Habit");
+      Key habitEntityKey = keyFactory.newKey(habitName);
+      datastore.update(habitEntityKey);
+    }
+}
