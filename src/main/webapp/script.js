fe6992a8bb0af8ae0a2063a564b99cb8c0ee7dc4
Hua Chen <huachen724@gmail.com>
2022-07-09 22:35:19 -0500

    Test 2.8

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -13,7 +13,7 @@ function loadHabits() {
 }
 
 function createHabitElement(habitName) {
-  habitNameString = JSON.stringify(habitName.listName);
+  habitNameString = JSON.stringify(habitName);
   var str = jsonObject.getString(habitName);
   const habitElement = document.createElement('li');
   habitElement.className = 'habit';
