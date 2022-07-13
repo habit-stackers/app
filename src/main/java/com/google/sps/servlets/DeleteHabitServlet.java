f67765196db47e7b45ee0305bdf1883f98e23873
Hua Chen <huachen724@gmail.com>
2022-07-12 21:49:58 -0500

    Test delete 2

--- src/main/java/com/google/sps/servlets/DeleteHabitServlet.java
+++ src/main/java/com/google/sps/servlets/DeleteHabitServlet.java
@@ -17,12 +17,29 @@ public class DeleteHabitServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      String habitName = request.getParameter("habitName");
-  
+      // Obtain the headers required for this route and store into habitNameString
+      String habitNameString = request.getParameter("habitName");
+      System.out.println("habitNameString: " + habitNameString);
+      System.out.println("request: " + request);
+      // Create an instance of datastore
       Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+      //
       KeyFactory keyFactory = datastore.newKeyFactory().setKind("HabitData");
-      Key habitEntityKey = keyFactory.newKey(habitName);
+      System.out.println("keyFactory: " + keyFactory);
+      // Create a key for the header
+      Key habitEntityKey = keyFactory.newKey(habitNameString);
+      System.out.println("habitEntityKey: " + habitEntityKey);
+      //Uses the key to delete the header
       datastore.delete(habitEntityKey);
+
+      /*
+          long id = Long.parseLong(request.getParameter("id"));
+          Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+          KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+          Key taskEntityKey = keyFactory.newKey(id);
+          datastore.delete(taskEntityKey);
+       */
     }
 }
   
