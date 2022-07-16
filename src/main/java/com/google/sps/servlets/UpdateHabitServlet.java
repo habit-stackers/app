5afe764f18ea149aef8a968d2536093aebd383bf
Hua Chen <huachen724@gmail.com>
2022-07-16 10:19:42 -0500

    Test 5 (seeing if unticking works)

--- src/main/java/com/google/sps/servlets/UpdateHabitServlet.java
+++ src/main/java/com/google/sps/servlets/UpdateHabitServlet.java
@@ -28,7 +28,10 @@ public class UpdateHabitServlet extends HttpServlet {
         Transaction transaction = datastore.newTransaction();
           Entity task = transaction.get(habitEntityKey);
           System.out.println("Entity task = transaction.get(habitEntityKey) : " + task );
-          if (task != null) {
+          if (task.getBoolean("isComplete")) {
+            transaction.put(Entity.newBuilder(task).set("isComplete", false).build());
+          }
+          else if (task.getBoolean("isComplete") == false) {
             transaction.put(Entity.newBuilder(task).set("isComplete", true).build());
           }
           transaction.commit();
